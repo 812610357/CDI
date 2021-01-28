41497840fb66457a2583d25f844151b4400f0477
812610357 <812610357@qq.com>
2021-01-28 22:46:49 +0800

    10*(20HIO+5ER)

--- core.py
+++ core.py
@@ -4,26 +4,53 @@ def FFT(data):
     '''
     傅里叶变换
     '''
-    return(np.fft.fftshift(np.fft.fft2(data)))
+    return(np.fft.fft2(data))
 
 def iFFT(data):
     '''
     傅里叶逆变换
     '''
-    return(np.fft.ifft2(np.fft.ifftshift(data)))
+    return(np.fft.ifft2(data))
 
 def ERcc(data,padding):
     '''
     ER算法的实空间约束条件
     '''
     data=data[padding[0]:-padding[0],padding[1]:-padding[1]]
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            if abs(data[i,j])<0:
-                data[i,j]==0
     data=np.pad(data,((padding[0],padding[1]),(padding[0],padding[1])),'constant')
     return(data)
+
+def HIOcc(data,temp,padding,beta):
+    '''
+    HIO算法的实空间约束条件
+    '''
+    inside=temp[padding[0]:-padding[0],padding[1]:-padding[1]]
+    insaide=np.pad(inside,((padding[0],padding[1]),(padding[0],padding[1])),'constant')
+    outside=data-beta*temp
+    for i in range(padding[0],outside.shape[0]-padding[0]):
+        for j in range(padding[1],outside.shape[1]-padding[1]):
+            outside[i,j]=0
+    return(insaide+outside) 
 
 def FScc(data,measurement):
     data=np.abs(measurement)*data/np.abs(data)
     return(data)
+
+def ER(realSpace,measurement,padding,t):
+    fourlieSpace=None
+    for i in range(t):
+        fourlieSpace=FFT(realSpace)
+        fourlieSpace=FScc(fourlieSpace,measurement)
+        realSpace=iFFT(fourlieSpace)
+        realSpace=ERcc(realSpace,padding)
+    return(realSpace)
+
+
+def HIO(realSpace,measurement,padding,beta,t):
+    fourlieSpace=None
+    for i in range(t):
+        fourlieSpace=FFT(realSpace)
+        fourlieSpace=FScc(fourlieSpace,measurement)
+        realSpaceTemp=iFFT(fourlieSpace)
+        realSpace=HIOcc(realSpace,realSpaceTemp,padding,beta)
+    return(realSpace)
--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import fileioput as fio
 import core
 import numpy as np
 import matplotlib.pyplot as plt
-path="./data/Lenna_test.png"
-oversamplingRatio=3
-data=fio.readimage("./data/Lenna.png")
-padding=np.array(np.floor(np.array(data.shape)*(oversamplingRatio-1)*0.5),dtype='int64')
-data=np.pad(data,((padding[0],padding[1]),(padding[0],padding[1])),'constant')
-projection=np.abs(core.FFT(data))
-#noise=np.random.poisson(projection.shape[0],)
 
-fourlieSpace=projection
-realSpace=np.zeros((projection.shape[0],projection.shape[1]))
-for i in range(1000):
-    realSpace=core.iFFT(fourlieSpace)
-    realSpace=core.ERcc(realSpace,padding)
-    fourlieSpace=core.FFT(realSpace)
-    fourlieSpace=core.FScc(fourlieSpace,projection)
+path = "./data/Lenna_test.png"
+oversamplingRatio = 3
+data = fio.readimage("./data/Lenna.png")
+padding = np.array(np.floor(np.array(data.shape) *
+                            (oversamplingRatio-1)*0.5), dtype='int64')
+data = np.pad(data, ((padding[0], padding[1]),
+                     (padding[0], padding[1])), 'constant')
+projection = np.abs(core.FFT(data))
+# noise=np.random.poisson(projection.shape[0],)
+
+fourlieSpace = projection
+realSpace = core.iFFT(fourlieSpace)
+
+for i in range(10):
+    realSpace = core.HIO(realSpace, projection, padding, 0.5, 20)
+    realSpace = core.ER(realSpace, projection, padding, 5)
 
 
-
-
-projection=np.abs(realSpace[padding[0]:-padding[0],padding[1]:-padding[1]])
-fio.showimage(projection)
-fio.writeimage(projection,"./data/Lenna_test.png")
+projection = np.abs(realSpace[padding[0]:-padding[0], padding[1]:-padding[1]])
+# fio.showimage(projection)
+fio.writeimage(projection, "./data/Lenna_test.png")
